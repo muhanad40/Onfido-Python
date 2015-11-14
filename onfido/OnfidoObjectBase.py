9673841033f68b27c72ce54ea79e62f00278e61e
Muhanad Al-Rubaiee <muhanadal-rubaiee@muhanads-mbp.home>
2015-11-14 23:35:44 +0000

    Object string representation

--- onfido/OnfidoObjectBase.py
+++ onfido/OnfidoObjectBase.py
@@ -5,6 +5,10 @@ class OnfidoObjectBase(object):
             data = args[0]
             self._add_to_object(data)
 
+    def __str__(self):
+    	cls_name = type(self).__name__
+    	return '<{0} id="{1}">'.format(cls_name, self.id)
+
     def _add_to_object(self, data):
         for item in data.items():
             setattr(self, item[0], item[1])
